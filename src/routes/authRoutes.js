48fc4bdbf390376b485a39e7e63d8c1838882f4c
Lenin-13 <mlenin922@gmail.com>
2026-05-27 17:08:02 -0500

    feat(auth): add request validation middleware using zod for register and login

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,10 +1,12 @@
 import express from "express";
 import { register, login } from "../controllers/authController.js";
+import { validateSchema } from "../middlewares/validatorMiddleware.js";
+import { registerSchema, loginSchema } from "../schemas/authSchemas.js";
 
 const router = express.Router();
 
-router.post("/register", register);
+router.post("/register", validateSchema(registerSchema), register);
 
-router.post("/login", login);
+router.post("/login", validateSchema(loginSchema), login);
 
 export default router;
